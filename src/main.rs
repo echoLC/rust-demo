5ea6244f7c6aecfa223fdaf3c49e3772100c67a5
luochao <luochao@joyy.com>
2022-01-25 11:48:00 +0800

    chore: 变量绑定和基本数据类型

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use num::complex::Complex;
+
+fn main() {
+    greet_world();
+    let a: i32 = 1;
+    let b: i32 = 3;
+    let c = 5_i32;
+    let mut d = 6i32;
+    println!("The value of d is: {}", d);
+    d = 7;
+    let e = add(add(a, b), add(c, d));
+    println!("(a + b) + (c + d) = {}", e);
+    let (x, mut y): (bool, bool) = (true, false);
+    println!("a = {:?}, b = {:?}", x, y);
+    y = true;
+    assert_eq!(x, y);
+    const MAX_POSITION: u32 = 100_000;
+    let spaces = "  ";
+    let spaces = spaces.len();
+    println!("Spaces length is {}", spaces);
+
+    let f = Complex { re: 2.1, im: -1.2 };
+    let h = Complex::new(11.1, 22.2);
+    let result = f + h;
+
+    println!("{} + {}i", result.re, result.im);
+}
+
 fn greet_world () {
     let southern_germany = "Grüß Gott!";
     let chinese = "世界，你好";
@@ -10,6 +38,6 @@ fn greet_world () {
     }
 }
 
-fn main() {
-    greet_world();
+fn add(i: i32, j: i32) -> i32 {
+    i + j
 }
