fa77ae8f29c352013676de0aeb0362aad5539eac
luochao <luochao@joyy.com>
2022-02-01 11:38:28 +0800

    feat: enum demo

--- src/main.rs
+++ src/main.rs
@@ -20,4 +20,37 @@ fn main() {
     };
 
     print!("{}", user2.active);
+
+    print_suit(PokerSuit::Diamonds);
+    print_suit(PokerSuit::Hearts);
+
+    let mut some = Some(5);
+    let absent_num: Option<u32> = None;
+    some = None;
+
+    println!("Option None:{:?}", some);
+
+    let six = plus_one(Some(5));
+    let none = plus_one(None);
+
+    println!("six:{:?},none:{:?}", six, none);
+}
+
+#[derive(Debug)]
+enum PokerSuit {
+    Clubs,
+    Spades,
+    Diamonds,
+    Hearts
+}
+
+fn print_suit (card: PokerSuit) {
+    println!("{:?}", card);
+}
+
+fn plus_one (x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) =>Some(i + 1)
+    }
 }
