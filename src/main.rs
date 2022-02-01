fb5dcb9b69faedb1430841316d7ebf579a64f635
luochao <luochao@joyy.com>
2022-02-01 10:40:24 +0800

    feat: struct demo

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,5 @@
 
 fn main() {
-    let s = String :: from("hello world");
-    let s1 = String :: from("中国");
-    let hello = &s[..5];   
-    let world = &s[6..];  
-    let one = &s1[..3];  
-    print!("{} {}", hello, world); 
-    print!("\n");
-    print!("{}", one); 
-
-    let mut s2 = String::new();
-    s2.push_str("hello world");
-    s2.push('!');
-    assert_eq!(s2, "hello world!");
-
-    let s3 = String::from("hello, world");
-    say_hello(&s3);
-    say_hello(&s3[..]);
-    say_hello(&s3.as_str());
-
-    print_str("中国人");
-
     struct User {
         active: bool,
         username: String,
@@ -41,14 +20,4 @@ fn main() {
     };
 
     print!("{}", user2.active);
-}
-
-fn say_hello(str: &str) {
-    print!("\n{}", str);
-}
-
-fn print_str(str: &str) {
-    for c in str.chars() {
-        print!("\n{}", c);
-    }
 }
