b680f7bd4e6d11cee46c2bb9389e199bda692f83
luochao <luochao@joyy.com>
2022-02-05 23:51:09 +0800

    feat: generics and trait demo

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,142 @@
-struct Circle {
-    x: f64,
-    y: f64,
-    radius: f64
+use std::fmt::Display;
+
+struct Point<T> {
+    x: T,
+    y: T
 }
 
-struct Rectangle {
-    width: u32,
-    height: u32
+impl <T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
 }
 
-impl Circle {
-    fn new (x: f64, y: f64, radius: f64) -> Circle {
-        Circle {
-            x,
-            y,
-            radius
+impl Point<f32> {
+    fn distance_from_origin (&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+pub trait Summary {
+    fn summarize_author (&self) -> String;
+    fn summarize (&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct Post {
+    pub title: String,
+    pub content: String,
+    pub author: String
+}
+
+impl Summary for Post {
+    fn summarize_author(&self) -> String {
+        format!("at {}", self.author)
+    }       
+}
+
+pub struct Twitter {
+    pub username: String,
+    pub content: String
+}
+
+impl Summary for Twitter {
+    fn summarize_author(&self) -> String {
+        format!("tweet {}", self.username)
+    }
+}
+
+pub struct Weibo {
+    pub username: String,
+    pub content: String
+}
+
+impl Summary for Weibo {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+pub fn notify (item: &impl Summary) {
+    println!("Break news!{}", item.summarize())   
+}
+
+pub fn notify1<T: Summary> (item: &T) {
+    println!("Break news!{}", item.summarize()) 
+}
+
+pub fn notify2<T: Summary + Display> (item: &T) {
+    println!("Break news!{}", item.summarize()) 
+}
+
+struct Pair<T> {
+    x: T,
+    y: T
+}
+
+impl <T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl <T: Display + PartialOrd> Pair<T> {
+    fn cmp_display (&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
         }
     }
-
-    fn area (&self) -> f64 {
-        std::f64::consts::PI * (self.radius * self.radius)
-    }
 }
 
-impl Rectangle {
-    fn area (&self) -> u32 {
-        self.width * self.height
-    }
+fn get_summarize () -> impl Summary {
+    Twitter{username: "sunface".to_string(),content: "Twitter 好用".to_string()} 
+}
 
-    fn width(&self) -> bool {
-        self.width > 0
+fn largest<T: PartialOrd + Copy> (list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item
+        }
     }
+    largest
 }
 
 fn main() {
-   let circle = Circle { x: 0.0, y: 0.0, radius: 20.0};
-   let area = circle.area();
-   println!("The area of circle which radius is 20 is: {}", area);
+    let point = Point {x: 2, y: 2};
+    let float = Point {x: 1.0, y: 4.0};
+    let f32: Point<f32> = Point {x: 1.0, y: 4.0};
 
-   let rect = Rectangle{ width: 30, height: 40};
-   println!("The area of the rectangle is {} square pixels.", rect.area());
-   if rect.width() {
-        println!("The with of the rectangle is positive.");
-   }
+    println!("x value is {}", point.x());
+    println!("float x value is {}", float.x);
+    println!("f32 distance value is {}", f32.distance_from_origin());
+
+    let post = Post{title: "Rust语言简介".to_string(),author: "Sunface".to_string(), content: "Rust棒极了!".to_string()};
+    let twitter = Twitter{username: "sunface".to_string(),content: "Twitter 好用".to_string()};
+    let weibo = Weibo{username: "sunface".to_string(),content: "好像微博没Tweet好用".to_string()};
+
+    println!("{}\n", post.summarize());
+    println!("{}\n", twitter.summarize());
+    println!("{}\n", weibo.summarize());
+    notify(&weibo);
+
+    let pair = Pair{x: 10, y: 9};
+    pair.cmp_display();
+
+    let summary = get_summarize();
+    println!("{}\n", summary.summarize());
+
+    let numbers = vec![1, 4, 6, 9, 3, 2, 5, 10];
+    let result = largest(&numbers);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
 
 
